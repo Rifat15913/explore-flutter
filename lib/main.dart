33b4361c920b38b4c7c681e5cc530d312de75e2f
Rifat <rifat@setuptech.international>
2019-10-01 16:30:24 +0600

    Learn building layout from codelabs

--- lib/main.dart
+++ lib/main.dart
@@ -29,15 +29,84 @@ class MyApp extends StatelessWidget {
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        GreenBox(),
-        GreenBox(),
-        GreenBox(),
-      ],
+    return Center(
+      child: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 50,
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        "Flutter McFlutter",
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .headline,
+                      ),
+                      Text(
+                        "Experienced Developer",
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .headline,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "123 Main Street",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline,
+                  ),
+                  Text(
+                    "(415) 555-0198",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(Icons.accessibility),
+                  Icon(Icons.timer),
+                  Icon(Icons.phone_android),
+                  Icon(Icons.phone_iphone),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
