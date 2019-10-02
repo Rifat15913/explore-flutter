a47965fee55eb70db8373e402cd7ef80f925c0cb
Rifat <rifat@setuptech.international>
2019-10-02 15:54:35 +0600

    Implement hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,7 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Startup Name Generator',
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordState createState() => RandomWordState();
-}
-
-class RandomWordState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return Divider();
-        }
-
-        final int index = i ~/ 2;
-
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: TextStyle(
-              fontSize: 18.0,
-            ),
-          ),
-        );
-      },
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    home: new Text("Hello World"),
+  ));
 }
